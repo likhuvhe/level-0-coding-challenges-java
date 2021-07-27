3bb3162b4410cc6888f70bf743dbf49e86aca007
likhuvhe <khuvhe.israel@gmail.com>
2021-07-27 20:55:20 +0200

    task 0.6

--- task06.java
+++ task06.java
@@ -15,13 +15,3 @@ class Task06 {
         return max;
     }
 }
-
-// maximum(num1, num2, num3):
-//     num_lst = [num1, num2, num3]
-//     maxm = num1
-//     index = 1
-//     while index < len(num_lst):
-//         if (num_lst[index] > maxm):
-//             maxm = num_lst[index]
-//         index +=1
-//     return maxm
