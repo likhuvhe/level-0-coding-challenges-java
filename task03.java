1280940bd130ac47ef3b58871e1bc8ab01da6b0e
likhuvhe <khuvhe.israel@gmail.com>
2021-07-27 18:08:36 +0200

    tas 1 to task 3

---
+++ task03.java
@@ -0,0 +1,9 @@
+class Task03{
+    public static void main(String[] args) {
+        hello("Tshepo");
+    }
+
+    public static void hello(String name) {
+        System.out.println("hello " + name + "!");        
+    }
+}
