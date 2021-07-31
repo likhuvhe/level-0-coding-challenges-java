0d5e11fdb58484181fe855517f53b72322a9ba40
likhuvhe <khuvhe.israel@gmail.com>
2021-07-31 11:12:31 +0200

    task10.java

--- task10.java
+++ task10.java
@@ -3,14 +3,16 @@ import java.util.*;
 class Task10{
     public static void main(String[] args) {
         commonLetters("house", "computers");
+        System.out.println("===");
         commonLetters("at", "computersa");
+        System.out.println("===");
         commonLetters("Eckard", "Berry");
 
     }
     public static void commonLetters(String string1, String string2) {
         string1 = string1.toLowerCase();
         string2 = string2.toLowerCase();
-        Set<String> commons = new HashSet<String>();
+        Set<String> commons = new LinkedHashSet<String>();
         for (int i = 0; i < string2.length(); i++){
             char letterStr2 = string2.charAt(i);
             for (int j = 0; j < string1.length(); j++){
